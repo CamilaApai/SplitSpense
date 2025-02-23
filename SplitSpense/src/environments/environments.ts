27aeef26b6da6cd389c23b6f8a58e51223bcbc39
Camila Apai <camilaapai@hotmail.com>
2025-02-23 03:29:20 -0300

    Set Up

---
+++ SplitSpense/src/environments/environments.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//const firebaseConfig 
+export const environment = {
+  production: false, 
+  firebaseConfig: {
+  apiKey: "AIzaSyAs8ugFZPKVNxJNOncx8ESEy_o2LQGlWAc",
+  authDomain: "splitspense-913a4.firebaseapp.com",
+  databaseURL: "https://splitspense-913a4-default-rtdb.firebaseio.com",
+  projectId: "splitspense-913a4",
+  storageBucket: "splitspense-913a4.firebasestorage.app",
+  messagingSenderId: "673193185582",
+  appId: "1:673193185582:web:9522ef9301f4ae08866abf",
+  measurementId: "G-9JH4Y2QJ54"
+}
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
